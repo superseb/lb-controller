67c8023869b25d8ca9b3ca12a819a45277e4623b
Sebastiaan van Steenis <mail@superseb.nl>
2018-02-07 10:45:11 +0100

    Use metadata IP instead of DNS name

--- package/rancher/entry.sh
+++ package/rancher/entry.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -ex
 
-while ! curl -s -f http://rancher-metadata/2015-12-19/stacks/Kubernetes/services/kubernetes/uuid; do
+while ! curl -s -f http://169.254.169.250/2015-12-19/stacks/Kubernetes/services/kubernetes/uuid; do
     echo Waiting for metadata
     sleep 1
 done
 
 /usr/bin/update-rancher-ssl
 
-UUID=$(curl -s http://rancher-metadata/2015-12-19/stacks/Kubernetes/services/kubernetes/uuid)
+UUID=$(curl -s http://169.254.169.250/2015-12-19/stacks/Kubernetes/services/kubernetes/uuid)
 ACTION=$(curl -s -u $CATTLE_ACCESS_KEY:$CATTLE_SECRET_KEY "$CATTLE_URL/services?uuid=$UUID" | jq -r '.data[0].actions.certificate')
 KUBERNETES_URL=${KUBERNETES_URL:-https://kubernetes:6443}
 
